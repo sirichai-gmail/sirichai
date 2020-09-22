49b0cbb2a9b03811d716cf25d09699143371dd98
sirichai-gmail <70198164+sirichai-gmail@users.noreply.github.com>
2020-09-22 13:01:55 +0700

    Add files via upload

---
+++ Untitled-1.java
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>CSS</h1>
+<h3>Inline CSS</h3>
+<h3 style="color:blue;">ศิริชัย สิงห์ศักดิ์ดี</h3>
+<p1 style="color:blue;">2010717102002</p1>
+<h3>Internal CSS</h3>
+<head>
+<style>
+body {background-color:powderblue;}
+h4   {color: blue;}
+p1    {color: green;}
+</style>
+</head>
+<h4>ศิริชัย สิงห์ศักดิ์ดี</h4>
+<p1>รหัสนักศึกษา 2010717102002</p1>
+<h3>External CSS</h3>
+<head>
+ <link rel="stylesheet" href="styles.css">
+<h1>ศิริชัย สิงห์ศักดิ์ดี</h1>
+<p2>ศิริชัย สิงห์ศักดิ์ดี</p2>
+</head>
+<h3>CSS Colors, Fonts and Sizes</h3>
+<head>
+<style>
+h6 {
+  color: green;
+  font-family: verdana;
+  font-size: 100%;
+}
+p2  {
+  color: brown;
+  font-family: courier;
+  font-size: 160%;
+}
+</style>
+</head>
+<h6>#ศิริชัย สิงห์ศักดิ์ดี</h6>
+<p2>2010717102002.</p2>
+<h3>CSS Border</h3>
+<head>
+<style>
+p {
+  border: 2px solid orange;
+}
+</style>
+</head>
+<h3>#ศิริชัย สิงห์ศักดิ์ดี</h3>
+<p>This is a paragraph.</p>
+<h3>CSS Padding</h3>
+<head>
+<style>
+p {
+  padding: 3px;
+}
+</style>
+</head>
+<h3>#ศิริชัย สิงห์ศักดิ์ดี</h3>
+<p>This is a paragraph.</p>
+<h3>CSS Margin</h3>
+<head>
+<style>
+p {
+  margin: 15px;
+}
+</style>
+</head>
+<h3>#ศิริชัย สิงห์ศักดิ์ดี</h3>
+<pre>
+ </pre>
+<h1>Links</h1>
+<h3>Links - Syntax</h3>
+<h3>#ศิริชัย สิงห์ศักดิ์ดี</h3>
+<a href="https://www.w3schools.com/">Visit W3Schools.com!</a>
+<h3>Links - The target Attribute</h3>
+<h3>ศิริชัย สิงห์ศักดิ์ดี</h3>
+<a href="https://www.youtube.com/watch?v=7iSia7rb1PY&ab_channel=Whattheduck" target="_blank"> Youtube!</a> 
+<h3>Absolute URLs vs. Relative URLs</h3>
+<h2>Absolute URLs ลิงค์หน้าเว็บไซต์</h2>
+<p1><a href="https://www.w3.org/">W3C</a></p1><br>
+<p1><a href="https://www.google.com/">Google</a></p1>
+<h2>Relative URLs ลิงค์เว็บไซต์หน้าต่างๆ</h2>
+<p1><a href="html_images.asp">HTML Images</a></p1><br>
+<p1><a href="/css/default.asp">CSS Tutorial</a></p1>
+<h3>Links - Use an Image as a Link</h3>
+<h3>#ศิริชัย สิงห์ศักดิ์ดี</h3>
+<a href="default.asp"><img src="smiley.gif" alt="HTML tutorial" style="width:42px;height:42px;"></a>
+<h3>Link to an Email Address</h3>
+<h2>#ศิริชัย สิงห์ศักดิ์ดี</h2>
+<p2><a href="mailto:someone@example.com">Send email</a></p2>
+<h3>Button as a Link</h3>
+<h3>#ศิริชัย สิงห์ศักดิ์ดี</h3>
+<button onclick="document.location='default.asp'">HTML Tutorial</button>
+<h3>Link Titles</h3>
+<p3>#ศิริชัย สิงห์ศักดิ์ดี</p3>
+<a href="https://www.w3schools.com/html/" title="ตรงนี้ Go to W3Schools HTML section">Visit our HTML Tutorial</a>
+<h3>Link Colors</h3>
+<head>
+<style>
+a:link {
+  color: green;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:visited {
+  color: orange;
+  background-color: transparent;
+  text-decoration: none;
+}
+a:hover {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+}
+a:active {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
+</head>
+<p3>เปลี่ยนสีของลิงค์ได้</p3>
+<a href="html_images.asp" target="_blank">HTML Images</a> 
+<h3>Link Buttons</h3>
+<p3>#ศิริชัย สิงห์ศักดิ์ดี</p3>
+<a href="default.asp" target="_blank">This is a link</a>
+<h3>Create a Bookmark in HTML</h3>
+<h3>#ศิริชัย สิงห์ศักดิ์ดี</h3>
+<p3><a href="#C4">Jump to Chapter 4</a></p3><br>
+<p3><a href="#C10">Jump to Chapter 10</a></p3><br>
+<p3><a href="#C19">Jump to Chapter 19</a></p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2 id="C4">Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2 id="C10">Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2>Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h2 id="C19">Chapter </h2>
+<p3>ศิริชัย สิงห์ศักดิ์ดี</p3>
+<h1>Images</h1>
+<h3>Image</h3>
+<h3>#เหล้าที่อาจารย์ชอบ</h3>
+<img src="https://www.bloggang.com/data/sulakkin/picture/1192210438.jpg" alt="เหล้าที่อาจารย์ชอบ" width="500" height="500">
+<h3>Tables</h3>
+<h2>ศิริชัย สิงห์ศักดิ์ดี</h2>
+<table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+  <tr>
+    <td>John</td>
+    <td>Doe</td>
+    <td>80</td>
+  </tr>
+</table>
+<h3>Lists</h3>
+<h2>ศิริชัย สิงห์ศักดิ์ดี</h2>
+<ul>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul>  
+<h3>Block and Inline Elements</h3>
+<div style="border: 1px solid black">ศิริชัย สิงห์ศักดิ์ดี</div>
+<p>The DIV element is a block element, and will always start on a new line and take up the full width available (stretches out to the left and right as far as it can).</p>
+<h3>class Attribute</h3>
+<head>
+<style>
+ city {
+  background-color: tomato;
+  color: white;
+  border: 2px solid black;
+  margin: 20px;
+  padding: 20px;
+}
+</style>
+</head>
+<body>
+<div class="city">
+<h2>London</h2>
+<p>London is the capital of England.</p>
+</div> 
+<div class="city">
+<h2>Paris</h2>
+<p>Paris is the capital of France.</p>
+</div>
+<div class="city">
+<h2>Tokyo</h2>
+<p>Tokyo is the capital of Japan.</p>
+</div>
+<h3>id Attribute</h3>
+<head>
+<style>
+#myHeader {
+  background-color: lightblue;
+  color: black;
+  padding: 40px;
+  text-align: center;
+}
+.city {
+  background-color: tomato;
+  color: white;
+  padding: 10px;
+} 
+</style>
+</head>
+<body>
+<h2>Difference Between Class and ID</h2>
+<p>A class name can be used by multiple HTML elements, while an id name must only be used by one HTML element within the page:</p>
+<h1 id="myHeader">ชัยธวัช ทองเพชรบูรพา</h1>
+<h2 class="city">ชัยธวัช ทองเพชรบูรพา</h2>
+<p>London is the capital of England.</p>
+<h2 class="city">ชัยธวัช ทองเพชรบูรพา</h2>
+<p>Paris is the capital of France.</p>
+<h2 class="city">ชัยธวัช ทองเพชรบูรพา</h2>
+<p>Tokyo is the capital of Japan.</p>
+<h3>Iframes</h3>
+<h2>ชัยธวัช ทองเพชรบูรพา</h2>
+<p>You can use the height and width attributes to specify the size of the iframe:</p>
+<iframe src="demo_iframe.htm" height="200" width="300" title="Iframe Example"></iframe>
+</body>
+</html>
