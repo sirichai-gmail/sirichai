d1fca18c7525412b7e1f6938cb1a1db92bcc0e43
sirichai-gmail <70198164+sirichai-gmail@users.noreply.github.com>
2020-12-01 15:28:58 +0700

    Add files via upload

---
+++ document.getElementById("SIRICAHI").styl.js
@@ -0,0 +1,39 @@
+document.getElementById("SIRICAHI").style.fontSize = "35px";
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Web Page</h1>
+<p>My First Paragraph</p>
+
+<p id="2010717102002"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+</script>
+
+</body>
+</html>
+var x, y, z;    // Statement 1
+x = 5;          // Statement 2
+y = 6;          // Statement 3
+z = x + y;      // Statement 4
+var x, y, z;       // Declare Variables
+x = 5; y = 6;      // Assign Values
+z = x + y;         // Compute Values
+// Change heading:
+document.getElementById("myH").innerHTML = "My First Page";
+
+// Change paragraph:
+document.getElementById("myP").innerHTML = "My first paragraph.";
+var x = 5;
+var y = 6;
+var z = x + y;
+var x = 5;         // assign the value 5 to x
+var y = 2;         // assign the value 2 to y
+var z = x + y;     // assign the value 7 to z (5 + 2)
+var x = 100 + 50;
+var x = 10;
+var length = 16;                               // Number
+var lastName = "Johnson";                      // String
+var x = {firstName:"John", lastName:"Doe"};    // Object
